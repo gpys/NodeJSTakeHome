8bb74cfbf6ed99b14ec913631e5c266b0e169a62
Jordan Jeter <jordan@jordans-mbp.lan>
2023-03-22 11:30:28 -0400

    define crypto route and create files for router and auth controller

---
+++ cryptoRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const crypto = express();
+
+crypto.use()
+
+
+
+
+export default cryptoRouter;
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+const cryptoRouter = require('./cryptoRouter');
+const auth = require('./controllers/authController');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 
 app.get('/', (req, res) => {
   res.send('Good luck!')
-})
+});
+
+//direct request to crypto endpoint to authentication and the appropriate router
+app.use('/crypto', auth, cryptoRouter);
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
-})
+});
